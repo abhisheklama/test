6c1015e6f6eadba9e3cc5d93aab67a61688926b9
diwanshu choudhary <diwanshu@visions.net.in>
2023-08-31 10:22:43 +0530

    removed master code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// console.log("Its master branch");
+console.log("master branch code been removed");
 // console.log("Its first commit");
 console.log("Its feature branch");
 console.log("feature intial commit");
 console.log("second commit");
-console.log("third commit")
+console.log("third commit");
