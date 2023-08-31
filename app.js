726307f69ecc59f2f87b0d45cafba460641db574
diwanshu choudhary <diwanshu@visions.net.in>
2023-08-31 10:10:56 +0530

    new Initial commit

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-console.log("Its master branch");
-console.log("Its first commit");
+// console.log("Its master branch");
+// console.log("Its first commit");
+console.log("Its feature branch");
+console.log("feature intial commit");
